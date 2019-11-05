dbc4107d86ecedda1cc37e46853c75536720ca18
raythurman2386 <raymondthurman5@gmail.com>
2019-11-04 21:40:47 -0500

    minor spacing changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-import { Route } from "react-router-dom";
-import SavedList from "./Movies/SavedList";
-import MovieList from "./Movies/MovieList";
-import Movie from "./Movies/Movie";
+import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
+import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -14,9 +14,9 @@ const App = () => {
   return (
     <>
       <SavedList list={savedList} />
-      <Route exact path="/" component={MovieList} />
+      <Route exact path='/' component={MovieList} />
       <Route
-        path="/movies/:id"
+        path='/movies/:id'
         render={props => {
           return <Movie {...props} addToSavedList={addToSavedList} />;
         }}
