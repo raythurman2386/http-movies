987d77f0974f7fe000cb51ad10e62bc38c711f47
raythurman2386 <raymondthurman5@gmail.com>
2019-11-05 19:11:25 -0500

    added withRouter, and update route

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
+import UpdateMovie from './Movies/UpdateMovie';
 import Movie from './Movies/Movie';
 
 const App = () => {
@@ -21,6 +22,7 @@ const App = () => {
           return <Movie {...props} addToSavedList={addToSavedList} />;
         }}
       />
+      <Route path='/update-movie/:id' component={UpdateMovie} />
     </>
   );
 };
