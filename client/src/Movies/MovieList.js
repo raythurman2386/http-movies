41a3eb368730879a289a3e0b0bf67dd8c38e89d3
raythurman2386 <raymondthurman5@gmail.com>
2019-11-07 21:48:02 -0500

    movielist refactored to hooks

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,60 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import MovieCard from './MovieCard';
 
-export default class MovieList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movies: []
-    };
-  }
-
-  componentDidMount() {
+const MovieList = ({ movies, deleteMovie, setMovies }) => {
+  useEffect(() => {
     axios
       .get('http://localhost:5000/api/movies')
-      .then(res => this.setState({ movies: res.data }))
+      .then(res => setMovies(res.data))
       .catch(err => console.log(err.response));
-  }
+  }, []);
 
-  deleteMovie = (e, id) => {
-    e.stopPropagation();
+  return (
+    <div className='movie-list'>
+      {movies.map(movie => (
+        <MovieDetails key={movie.id} movie={movie} deleteMovie={deleteMovie} />
+      ))}
+    </div>
+  );
+};
 
-    // save the movie in case of error with api
-    const removedMovie = this.state.movies.find(movie => movie.id === id);
-
-    // make sure the user confirms delete
-    if (window.confirm('Are you sure you want to delete this movie')) {
-      // Set movies to the new updated list without deleted movie
-      this.setState(this.state.movies.filter(movie => movie.id !== id));
-
-      // api call to delete movie
-      axios
-        .delete(`http://localhost:5000/api/movies/${id}`)
-        .then(res => console.log(res))
-        .catch(err => {
-          console.log(err);
-          // Re add the movie if there is an error
-          this.setState([...this.state.movies, removedMovie]);
-        });
-    }
-  };
-
-  render() {
-    return (
-      <div className='movie-list'>
-        {this.state.movies.map(movie => (
-          <MovieDetails
-            key={movie.id}
-            movie={movie}
-            deleteMovie={this.deleteMovie}
-          />
-        ))}
-      </div>
-    );
-  }
-}
+export default MovieList;
 
 function MovieDetails({ movie, deleteMovie }) {
   return (
